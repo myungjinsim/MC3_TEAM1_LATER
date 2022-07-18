41e1f355ea1c001335d722590020cba2669483a9
phainestha1 <250wons@gmail.com>
2022-07-19 02:23:22 +0900

    [Add] Home / HomeViewController 생성 [#5]

---
+++ RoomEscape/RoomEscape/ViewControllers/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  RoomEscape
+//
+//  Created by Noah's Ark on 2022/07/19.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}// HomeViewController
