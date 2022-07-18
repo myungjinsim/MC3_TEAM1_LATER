58b6c69cd913668ce8921bbe92d69b694c4d97bc
otoolz <otool.hf@gmail.com>
2022-07-18 15:22:11 +0900

    [Add] #3 ThemeCompare 관련 파일 생성
    ThemeCompare Storyboard 생성
    ThemeCompareViewController.swift 생성

---
+++ RoomEscape/RoomEscape/ViewControllers/ThemeCompareViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ThemeCompareViewController.swift
+//  RoomEscape
+//
+//  Created by KiWoong Hong on 2022/07/18.
+//
+
+import UIKit
+
+class ThemeCompareViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
