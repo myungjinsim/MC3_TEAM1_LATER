6ab7bd404a0e2c05f632fe2200300250aea2dff0
mxnxxii <m990508m@gmail.com>
2022-07-18 16:12:20 +0900

    [Style] Color Set 설정

---
+++ RoomEscape/RoomEscape/Extensions/Extension.swift
@@ -0,0 +1,31 @@
+//
+//  Extension.swift
+//  RoomEscape
+//
+//  Created by MINJI on 2022/07/18.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    // 피그마에 등록된 색상 보시고, 사용하시면 됩니다!
+    //self.myButton.backgroundColor = UIColor.titleWhite 혹은 self.myButton.backgroundColor = .titleWhite 이런식으로 사용
+    
+
+    // Title Color
+    static let titleWhite = UIColor(named: "text1")
+    static let titleLightgray = UIColor(named: "text2")
+    static let titleGray = UIColor(named: "text3")
+    static let titleDarkGray = UIColor(named: "text4")
+    static let titleBlack = UIColor(named: "text5")
+
+    // Main Color
+    static let mainPurple = UIColor(named: "main")
+    
+    //etc
+    static let background = UIColor(named: "background")
+    static let background2 = UIColor(named: "background2")
+    
+}
+
