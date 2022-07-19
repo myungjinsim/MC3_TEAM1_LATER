1f8e0444671ab2e376383e4ca46da995fae727f0
otoolz <otool.hf@gmail.com>
2022-07-19 17:45:55 +0900

    [Feat] #3 ThemeCompare View / ViewController 연결
    테마 비교하기 화면에서
    첫번째와 두번재 테마 제목, 장르, 난이도, 시간제한, 최대인원, 활동성 라벨과 테마 이미지 IBOutlet 연결

--- RoomEscape/RoomEscape/ViewControllers/ThemeCompareViewController.swift
+++ RoomEscape/RoomEscape/ViewControllers/ThemeCompareViewController.swift
@@ -9,21 +9,23 @@ import UIKit
 
 class ThemeCompareViewController: UIViewController {
 
+    @IBOutlet weak var firstThemeTitle: UILabel!
+    @IBOutlet weak var firstThemeImage: UIImageView!
+    @IBOutlet weak var firstThemeGenre: UILabel!
+    @IBOutlet weak var firstThemeDifficulty: UILabel!
+    @IBOutlet weak var firstThemeActivity: UILabel!
+    @IBOutlet weak var firstThemeMaxParty: UILabel!
+    @IBOutlet weak var firstThemeTimeLimit: UILabel!
+    
+    @IBOutlet weak var secondThemeTitle: UILabel!
+    @IBOutlet weak var secondThemeImage: UIImageView!
+    @IBOutlet weak var secondThemeGenre: UILabel!
+    @IBOutlet weak var secondThemeDifficulty: UILabel!
+    @IBOutlet weak var secondThemeActivity: UILabel!
+    @IBOutlet weak var secondThemeMaxParty: UILabel!
+    @IBOutlet weak var secondThemeTimeLimit: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
