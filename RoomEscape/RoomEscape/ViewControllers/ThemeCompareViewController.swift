5f0a6962a49f1e65f471a6a5e3aa68d3ffe6de84
otoolz <otool.hf@gmail.com>
2022-07-20 01:55:02 +0900

    [Feat] #3 테마 비교 화면 dummy 데이터 추가
    테마 비교 화면 dummy 데이터 추가

--- RoomEscape/RoomEscape/ViewControllers/ThemeCompareViewController.swift
+++ RoomEscape/RoomEscape/ViewControllers/ThemeCompareViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class ThemeCompareViewController: UIViewController {
+    
+    let firstTheme: SampleModel = dummys[0]
+    let secondTheme: SampleModel = dummys[1]
 
     @IBOutlet weak var firstThemeTitle: UILabel!
     @IBOutlet weak var firstThemeImage: UIImageView!
@@ -28,4 +31,22 @@ class ThemeCompareViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+}
+
+struct SampleModel {
+    let title: String
+    let image: String
+    let genre: String
+    let difficulty: Float
+    let activity: String
+    let maxParty: Int
+    let timeLimit: Int
+    let description: String
+}
+    
+var dummys: [SampleModel] {
+    [
+        SampleModel(title: "낭랑카페", image: "", genre: "미스테리", difficulty: 4.0, activity: "많음", maxParty: 6, timeLimit: 75, description: "안쓰는 디스크립션"),
+        SampleModel(title: "안개꽃", image: "", genre: "감성", difficulty: 3.0, activity: "적음", maxParty: 4, timeLimit: 60, description: "안쓰는 디스크립션")
+    ]
 }
